bc9a10b442217583975608cfe7eba279755c7820
Sadegh Hamedani <sad3gh.hmp@gmail.com>
2022-02-21 17:03:39 +0330

    backend: add movie model

--- backend/app/models.py
+++ backend/app/models.py
@@ -1,6 +1,15 @@
 from django.db import models
 
+from django.contrib.postgres.fields import ArrayField
 from django.contrib.auth.models import AbstractUser
+
+
+class Movie(models.Model):
+    title = models.CharField(max_length=100)
+    year = models.CharField(max_length=4, default="1900")
+    imdbid = models.CharField(max_length=20, blank=True)
+    tmdbid = models.CharField(max_length=20, blank=True)
+    genres = ArrayField(models.CharField(max_length=10))
 
 
 class User(AbstractUser):
@@ -12,3 +21,6 @@ class User(AbstractUser):
     bio = models.TextField(blank=True)
     location = models.CharField(max_length=20, blank=True)
     gender = models.CharField(max_length=1, choices=GENDER_CHOICES)
+    friends = models.ManyToManyField("self")
+    watchlist = models.ManyToManyField(Movie, related_name='watchlist')
+    liked = models.ManyToManyField(Movie, related_name='liked')
