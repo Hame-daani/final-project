408fb81bd45a00cb7bc467d76c104c355025b7e1
Sadegh Hamedani <sad3gh.hmp@gmail.com>
2022-02-24 16:53:52 +0330

    backend: bug in timed

--- backend/app/utils.py
+++ backend/app/utils.py
@@ -8,7 +8,8 @@ def timed(func):
     def wrapper(*args, **kwargs):
         start = time.time()
         print(f'\n-----{func.__name__} started-----')
-        func(*args, **kwargs)
+        result = func(*args, **kwargs)
         print(
             f"\n-----{func.__name__} finished in {time.time() - start:.2f} seconds-----")
+        return result
     return wrapper
