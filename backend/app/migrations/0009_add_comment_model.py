c4af7ceb2be06930370bcf0148da292f5c24cf4e
Sadegh Hamedani <sad3gh.hmp@gmail.com>
2022-02-25 20:36:10 +0330

    backend: rename load_friends migration

--- backend/app/migrations/0009_add_comment_model.py
+++ backend/app/migrations/0009_add_comment_model.py
@@ -8,7 +8,7 @@ import django.db.models.deletion
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('app', '0008_load_friends'),
+        ('app', '0008_load_friends_data'),
     ]
 
     operations = [
