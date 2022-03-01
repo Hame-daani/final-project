2ab25e41714e25e815a6daaa6db8e2014bdc0678
Sadegh Hamedani <sad3gh.hmp@gmail.com>
2022-03-01 18:38:38 +0330

    backend: register models in admin

--- backend/app/admin.py
+++ backend/app/admin.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 
-from .models import User
+from .models import *
 
 admin.site.register(User, UserAdmin)
+admin.site.register(Movie)
+admin.site.register(Review)
+admin.site.register(FriendRequest)
+admin.site.register(Comment)
+admin.site.register(Like)
