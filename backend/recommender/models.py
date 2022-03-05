d84bf7b976fcb6b4785d58e810f8734a1c5e79fb
Sadegh Hamedani <sad3gh.hmp@gmail.com>
2022-03-05 14:38:20 +0330

    [backend/recommender] alter fields

--- backend/recommender/models.py
+++ backend/recommender/models.py
@@ -4,14 +4,16 @@ from django.contrib.contenttypes.models import ContentType
 
 
 class Similarity(models.Model):
-    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    content_type = models.ForeignKey(
+        ContentType, on_delete=models.CASCADE, related_name="similarities"
+    )
     source_id = models.PositiveIntegerField()
     source = GenericForeignKey("content_type", "source_id")
 
     target_id = models.PositiveIntegerField()
     target = GenericForeignKey("content_type", "target_id")
 
-    score = models.DecimalField(max_digits=3, decimal_places=2)
+    score = models.DecimalField(max_digits=4, decimal_places=3)
 
     def __str__(self) -> str:
-        return f"{self.content_type.model}: {self.source_id} and {self.target_id} score {self.score:.2f}"
+        return f"{self.content_type.model}: {self.source_id} and {self.target_id} score {self.score:.3f}"
