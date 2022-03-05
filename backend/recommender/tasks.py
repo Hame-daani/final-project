1d644b9cb3d14a99949e794c23160bdc0d5615dc
Sadegh Hamedani <sad3gh.hmp@gmail.com>
2022-03-05 19:39:50 +0330

    [backend/recommender] generate_mm_data finished

--- backend/recommender/tasks.py
+++ backend/recommender/tasks.py
@@ -1,4 +1,6 @@
 from datetime import datetime
+import logging
+
 from celery import shared_task
 from config.celery import app
 from celery.schedules import crontab
@@ -9,7 +11,7 @@ from recommender.module import sim_pearson
 from recommender.models import Similarity
 from app.models import User, Movie
 
-tasks_time = crontab(minute=30)
+tasks_time = crontab(minute=36)
 
 app.conf.beat_schedule = {
     "Generate_User-User_Data": {
@@ -25,17 +27,40 @@ app.conf.beat_schedule = {
 
 @shared_task
 def generate_mm_data():
-    return
+    logging.error("deleting data")
+    ct = ContentType.objects.get_for_model(Movie)
+    Similarity.objects.filter(content_type=ct).delete()
+    start = datetime.now()
+    logging.error("start mm_data")
+    movies = Movie.objects.order_by("id")[:100]
+    for movie1 in movies:
+        for movie2 in movies:
+            if movie1 == movie2:
+                continue
+            else:
+                try:
+                    obj = Similarity.objects.get(
+                        content_type=ct,
+                        source_id=movie1.id,
+                        target_id=movie2.id,
+                    )
+                    continue
+                except:
+                    Similarity.objects.create(
+                        source=movie1, target=movie2, score=sim_pearson(movie1, movie2)
+                    )
+        logging.error(f"movie{movie1.id} done!")
+    finish = datetime.now()
+    logging.error(f"task mm_data finsihed in {finish-start}")
 
 
 @shared_task
 def generate_uu_data():
-    print("deleting data")
-    Similarity.objects.filter(
-        content_type=ContentType.objects.get_for_model(User)
-    ).delete()
+    logging.warning("deleting data")
+    ct = ContentType.objects.get_for_model(User)
+    Similarity.objects.filter(content_type=ct).delete()
     start = datetime.now()
-    print("start uu_data")
+    logging.warning("start uu_data")
     users = User.objects.order_by("id")[:100]
     for user1 in users:
         for user2 in users:
@@ -44,7 +69,7 @@ def generate_uu_data():
             else:
                 try:
                     obj = Similarity.objects.get(
-                        content_type=ContentType.objects.get_for_model(user1),
+                        content_type=ct,
                         source_id=user1.id,
                         target_id=user2.id,
                     )
@@ -53,6 +78,6 @@ def generate_uu_data():
                     Similarity.objects.create(
                         source=user1, target=user2, score=sim_pearson(user1, user2)
                     )
-        print(f"user{user1.id} done!")
+        logging.warning(f"user{user1.id} done!")
     finish = datetime.now()
-    print(f"task uu_data finsihed in {finish-start}")
+    logging.warning(f"task uu_data finsihed in {finish-start}")
