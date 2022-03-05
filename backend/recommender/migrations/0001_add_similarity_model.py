be9d13d64de341c4a2b7404b50c291bddc6f6ef3
Sadegh Hamedani <sad3gh.hmp@gmail.com>
2022-03-05 11:14:55 +0330

    backend: similarity model score field fixed

--- backend/recommender/migrations/0001_add_similarity_model.py
+++ backend/recommender/migrations/0001_add_similarity_model.py
@@ -19,7 +19,7 @@ class Migration(migrations.Migration):
                 ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
                 ('source_id', models.PositiveIntegerField()),
                 ('target_id', models.PositiveIntegerField()),
-                ('score', models.SmallIntegerField()),
+                ('score', models.DecimalField(decimal_places=2, max_digits=3)),
                 ('content_type', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='contenttypes.contenttype')),
             ],
         ),
