1e19c658932d556f34f8bebcb70abc259e71e414
Sadegh Hamedani <sad3gh.hmp@gmail.com>
2022-03-05 11:28:32 +0330

    [backend/migrations] user 5000 bug fixed

--- backend/generate_users_data.py
+++ backend/generate_users_data.py
@@ -12,7 +12,7 @@ def get_data():
     max_id = 5000
     min_id_movie = 1
     max_id_movie = 999
-    missing_ids = [
+    missing_movie_ids = [
         91,
         221,
         291,
@@ -34,14 +34,14 @@ def get_data():
         883,
         888,
         978,
-        995
+        995,
     ]
     faker = Faker()
     Faker.seed(0)
     faker.add_provider(address)
     faker.add_provider(person)
     faker.add_provider(lorem)
-    for i in range(1, 5000):
+    for i in range(min_id, max_id + 1):
         first_name = faker.first_name()
         last_name = faker.last_name()
         username = first_name+last_name+str(i)
@@ -61,14 +61,14 @@ def get_data():
         watchlist = []
         for _ in range(n):
             pk = randint(min_id_movie, max_id_movie)
-            if pk != i and pk not in missing_ids:
+            if pk != i and pk not in missing_movie_ids:
                 watchlist.append(pk)
         # favorites
         n = randint(1, 100)
         favorites = []
         for _ in range(n):
             pk = randint(min_id_movie, max_id_movie)
-            if pk != i and pk not in missing_ids:
+            if pk != i and pk not in missing_movie_ids:
                 favorites.append(pk)
         users.append(
             {
