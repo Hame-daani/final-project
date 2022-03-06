a27712d69f2cdb36f3a07d724dc6907a38fda716
Sadegh Hamedani <sad3gh.hmp@gmail.com>
2022-03-06 15:10:25 +0330

    [backend/recommender] algorithm faster now. 100 user in 30 sec

--- backend/recommender/module.py
+++ backend/recommender/module.py
@@ -2,41 +2,22 @@ from math import sqrt
 from app.models import User, Movie
 
 
-def get_ratings(p1, p2):
-    type_p1 = type(p1)
-    type_p2 = type(p2)
-    if type_p1 != type_p2:
-        raise Exception("got two diffrent thing!")
-    if type_p1 == Movie:
-        p2_reviews = p2.reviews.filter(
-            user__id__in=p1.reviews.values_list("user", flat=True)
-        )
-        p1_reviews = p1.reviews.filter(
-            user__id__in=p2_reviews.values_list("user", flat=True)
-        )
-        p1_ratings = p1_reviews.order_by("user").values_list("rating", flat=True)
-        p2_ratings = p2_reviews.order_by("user").values_list("rating", flat=True)
-    if type_p1 == User:
-        p2_reviews = p2.reviews.filter(
-            movie__id__in=p1.reviews.values_list("movie", flat=True)
-        )
-        p1_reviews = p1.reviews.filter(
-            movie__id__in=p2_reviews.values_list("movie", flat=True)
-        )
-        p1_ratings = p1_reviews.order_by("movie").values_list("rating", flat=True)
-        p2_ratings = p2_reviews.order_by("movie").values_list("rating", flat=True)
+def get_ratings(p1, p2, data):
+    similars = list(set([t for t, r in data[p1]]) & set([t for t, r in data[p2]]))
+    p1_ratings = [r for t, r in data[p1] if t in similars]
+    p2_ratings = [r for t, r in data[p2] if t in similars]
     return p1_ratings, p2_ratings
 
 
-def sim_pearson(p1, p2):
-    p1_ratings, p2_ratings = get_ratings(p1, p2)
+def sim_pearson(p1, p2, data):
+    p1_ratings, p2_ratings = get_ratings(p1, p2, data)
     n = len(p1_ratings)
     if n == 0:
         return 0
     sum1 = sum(p1_ratings)
     sum2 = sum(p2_ratings)
-    sum1sq = sum([pow(rating, 2) for rating in p1_ratings])
-    sum2sq = sum([pow(rating, 2) for rating in p2_ratings])
+    sum1sq = sum([rating**2 for rating in p1_ratings])
+    sum2sq = sum([rating**2 for rating in p2_ratings])
     psum = sum([p1_ratings[i] * p2_ratings[i] for i in range(n)])
     num = psum - (sum1 * sum2 / n)
     den = sqrt((sum1sq - pow(sum1, 2) / n) * (sum2sq - pow(sum2, 2) / n))
@@ -62,5 +43,4 @@ class FriendsRecommender:
                 total += friend_sim * review.rating
             except:
                 continue
-        user.similarities
         return round(total / sim_sum, 1)
--- backend/recommender/tasks.py
+++ backend/recommender/tasks.py
@@ -28,67 +28,67 @@ app.conf.beat_schedule = {
 
 @shared_task
 def generate_mm_data():
+    Similarity.truncate()
     ct = ContentType.objects.get_for_model(Movie)
-    start = datetime.now()
-    logging.error("start mm_data")
     movies = Movie.objects.annotate(rcount=Count("reviews__id"))
-    # 331 movies
-    movies = movies.filter(rcount__gt=100)
-    n = len(movies)
-    logging.error(f"calculating for {n} movies")
-    for i, movie1 in enumerate(movies):
-        if movie1.similarities.count() == (n - 1):
-            logging.error(f"movie {movie1.id} already done!")
-            continue
-        for movie2 in movies:
-            if movie1 == movie2:
+    # 450 movies
+    movies = movies.filter(rcount__gt=50)[:100]
+    data = {
+        movie.id: list(movie.reviews.values_list("user", "rating")) for movie in movies
+    }
+    movie_ids = list(movies.values_list("id", flat=True))
+    n = len(movie_ids)
+    start = datetime.now()
+    logging.warning("start mm_data")
+    logging.warning(f"calculating for {n} movies")
+    for i, movie1 in enumerate(movie_ids):
+        # if similarities.count() == (n - 1):
+        #     logging.warning(f"movie {movie1} already done!")
+        #     continue
+        for movie2 in movie_ids:
+            if movie1 >= movie2:
                 continue
             else:
-                try:
-                    obj = Similarity.objects.get(
-                        content_type=ct,
-                        source_id=movie1.id,
-                        target_id=movie2.id,
-                    )
-                    continue
-                except:
-                    Similarity.objects.create(
-                        source=movie1, target=movie2, score=sim_pearson(movie1, movie2)
-                    )
-        logging.error(f"movie {i} done!")
+                Similarity.objects.create(
+                    content_type=ct,
+                    source_id=movie1,
+                    target_id=movie2,
+                    score=sim_pearson(movie1, movie2, data),
+                )
+        logging.warning(f"movie {i+1} done!")
     finish = datetime.now()
     logging.error(f"task mm_data finsihed in {finish-start}")
 
 
 @shared_task
 def generate_uu_data():
+    Similarity.truncate()
     ct = ContentType.objects.get_for_model(User)
-    start = datetime.now()
-    logging.warning("start uu_data")
     users = User.objects.annotate(rcount=Count("reviews__id"))
     # 320 users
-    users = users.filter(rcount__gt=100)
-    n = len(users)
+    users = users.filter(rcount__gt=10)[:100]
+    data = {
+        user.id: list(user.reviews.values_list("movie", "rating")) for user in users
+    }
+    user_ids = list(users.values_list("id", flat=True))
+    n = len(user_ids)
+    start = datetime.now()
+    logging.warning("start uu_data")
     logging.warning(f"calculating for {n} users")
-    for i, user1 in enumerate(users):
-        if user1.similarities.count() == (n - 1):
-            logging.warning(f"user {user1.id} already done!")
-            continue
-        for user2 in users:
-            if user1 == user2:
+    for i, user1 in enumerate(user_ids):
+        # if user1.similarities.count() == (n - 1):
+        #     logging.warning(f"user {user1.id} already done!")
+        #     continue
+        for user2 in user_ids:
+            if user1 >= user2:
                 continue
             else:
-                try:
-                    obj = Similarity.objects.get(
-                        content_type=ct,
-                        source_id=user1.id,
-                        target_id=user2.id,
-                    )
-                    continue
-                except:
-                    Similarity.objects.create(
-                        source=user1, target=user2, score=sim_pearson(user1, user2)
-                    )
-        logging.warning(f"user {i} done!")
+                Similarity.objects.create(
+                    content_type=ct,
+                    source_id=user1,
+                    target_id=user2,
+                    score=sim_pearson(user1, user2, data),
+                )
+        logging.warning(f"user {i+1} done!")
     finish = datetime.now()
     logging.warning(f"task uu_data finsihed in {finish-start}")
