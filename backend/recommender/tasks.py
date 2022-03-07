aad3e19a7726bad024adf208e02ff211db1b4647
Sadegh Hamedani <sad3gh.hmp@gmail.com>
2022-03-07 12:32:18 +0330

    [backend/recommender/tasks] some refactoring

--- backend/recommender/tasks.py
+++ backend/recommender/tasks.py
@@ -35,24 +35,38 @@ class Ttype(Enum):
     odd_even = 3
 
 
+def isodd(n):
+    return n % 2 != 0
+
+
+def iseven(n):
+    return n % 2 == 0
+
+
 @shared_task
 def generate_mm_data():
     Similarity.truncate()
     ct = ContentType.objects.get_for_model(Movie)
     movies = Movie.objects.annotate(rcount=Count("reviews__id"))
     # 450 movies
-    movies = movies.filter(rcount__gt=50)[:100]
+    movies = movies.filter(rcount__gt=50)
     data = {
         movie.id: list(movie.reviews.values_list("user", "rating")) for movie in movies
     }
     movie_ids = list(movies.values_list("id", flat=True))
     n = len(movie_ids)
-    logging.warning(f"start mm_data with {Similarity.objects.count()}")
+    logging.warning(
+        f"start mm_data with {Similarity.objects.filter(content_type=ct).count()}"
+    )
     start = datetime.now()
     logging.warning(f"calculating for {n} movies")
-    t1 = threading.Thread(target=calculating, args=(ct, movie_ids, data, 1))
-    t2 = threading.Thread(target=calculating, args=(ct, movie_ids, data, 2))
-    t3 = threading.Thread(target=calculating, args=(ct, movie_ids, data, 3))
+    t1 = threading.Thread(target=calculating, args=(ct, movie_ids, data, Ttype.odd_odd))
+    t2 = threading.Thread(
+        target=calculating, args=(ct, movie_ids, data, Ttype.odd_even)
+    )
+    t3 = threading.Thread(
+        target=calculating, args=(ct, movie_ids, data, Ttype.even_even)
+    )
     t1.start()
     t2.start()
     t3.start()
@@ -61,7 +75,7 @@ def generate_mm_data():
     t3.join()
     finish = datetime.now()
     logging.error(
-        f"task mm_data finsihed in {finish-start} with {Similarity.objects.count()}"
+        f"task mm_data finsihed in {finish-start} with {Similarity.objects.filter(content_type=ct).count()}"
     )
 
 
@@ -70,14 +84,16 @@ def generate_uu_data():
     Similarity.truncate()
     ct = ContentType.objects.get_for_model(User)
     users = User.objects.annotate(rcount=Count("reviews__id"))
-    # 320 users
+    # 1014 users
     users = users.filter(rcount__gt=10)[:100]
     data = {
         user.id: list(user.reviews.values_list("movie", "rating")) for user in users
     }
     user_ids = list(users.values_list("id", flat=True))
     n = len(user_ids)
-    logging.warning(f"start uu_data {Similarity.objects.count()}")
+    logging.warning(
+        f"start uu_data {Similarity.objects.filter(content_type=ct).count()}"
+    )
     start = datetime.now()
     logging.warning(f"calculating for {n} users")
     t1 = threading.Thread(target=calculating, args=(ct, user_ids, data, Ttype.odd_odd))
@@ -93,38 +109,36 @@ def generate_uu_data():
     t3.join()
     finish = datetime.now()
     logging.warning(
-        f"finished uu_data in {finish-start} with {Similarity.objects.count()}"
+        f"finished uu_data in {finish-start} with {Similarity.objects.filter(content_type=ct).count()}"
     )
 
 
 def calculating(ct, ids, data, ttype=1):
-    connection.connect()
+    # connection.connect()
+    c = 0
     for i, t1 in enumerate(ids):
         # if user1.similarities.count() == (n - 1):
         #     logging.warning(f"user {user1.id} already done!")
         #     continue
-        if ttype == Ttype.odd_odd:
-            if i % 2 == 0:
+        if ttype in [Ttype.odd_odd, Ttype.odd_even]:
+            if iseven(i):
                 continue
         if ttype == Ttype.even_even:
-            if i % 2 != 0:
+            if isodd(i):
                 continue
         for j, t2 in enumerate(reversed(ids)):
-            if i == j:
+            if i == j and ttype in [Ttype.odd_odd, Ttype.even_even]:
                 break
-            if ttype == Ttype.odd_odd and j % 2 == 0:
+            if iseven(j) and ttype == Ttype.odd_odd:
                 continue
-            if ttype == Ttype.even_even and j % 2 != 0:
+            if isodd(j) and ttype in [Ttype.even_even, Ttype.odd_even]:
                 continue
-            if ttype == Ttype.odd_even:
-                if i % 2 != 0 and j % 2 != 0:
-                    continue
-                if i % 2 == 0 and j % 2 == 0:
-                    continue
+            c += 1
             Similarity.objects.create(
                 content_type=ct,
                 source_id=t1,
                 target_id=t2,
                 score=sim_pearson(t1, t2, data),
             )
-        print(f"worker:{ttype.name} {ct.model}-{i} done!")
+        # print(f"worker:{ttype.name} {ct.model}-{i} done!")
+    print(f"worker:{ttype.name} counter: {c}")
