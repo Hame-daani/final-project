3a4347b1e65e79f2a576a1779ab8c4d99cd5daf5
Sadegh Hamedani <sad3gh.hmp@gmail.com>
2022-04-16 17:04:42 +0430

    [backend] loginview added

--- backend/app/views/auth.py
+++ backend/app/views/auth.py
@@ -3,6 +3,8 @@ from rest_framework.generics import GenericAPIView, CreateAPIView
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework.response import Response
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.authtoken.models import Token
 
 from app.serializers import UserSerializer
 
@@ -19,3 +21,12 @@ class LogoutAPIView(GenericAPIView):
 class UserRegistration(CreateAPIView):
     serializer_class = UserSerializer
     permission_classes = (AllowAny,)
+
+
+class LoginView(ObtainAuthToken):
+    def post(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data["user"]
+        token, created = Token.objects.get_or_create(user=user)
+        return Response({"token": token.key, "user": UserSerializer(user).data})
