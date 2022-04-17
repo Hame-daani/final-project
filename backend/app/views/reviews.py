3701b0da7b67e1a7ddcfa5ddabd89c2d72c59234
Sadegh Hamedani <sad3gh.hmp@gmail.com>
2022-04-17 21:59:48 +0430

    [backend/api] recent reviews added

--- backend/app/views/reviews.py
+++ backend/app/views/reviews.py
@@ -2,6 +2,7 @@ from rest_framework.viewsets import ModelViewSet
 from rest_framework import filters
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.decorators import action
 
 from django_filters.rest_framework import DjangoFilterBackend
 
@@ -27,3 +28,24 @@ class ReviewViewSet(ModelViewSet, Commentable, Likeable):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    @action(detail=False, methods=["GET"])
+    def recent(self, request):
+        queryset = Review.objects.order_by("-created_at")[:10]
+        serializer = self.serializer_class(queryset, many=True)
+        return Response(serializer.data)
+
+    @action(detail=False, methods=["GET"])
+    def friends(self, request):
+        user = request.user
+        if user.is_authenticated:
+            friends = user.friends.all()
+            queryset = Review.objects.filter(user__in=friends).order_by("-created_at")[
+                :10
+            ]
+            serializer = self.serializer_class(queryset, many=True)
+            return Response(serializer.data)
+        else:
+            return Response(
+                data={"you need to sign in"}, status=status.HTTP_401_UNAUTHORIZED
+            )
