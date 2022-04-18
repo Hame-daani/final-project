1e4f8ac103ab07eee32e32e94ea1f6a5131c2ce6
Sadegh Hamedani <sad3gh.hmp@gmail.com>
2022-04-18 13:33:22 +0430

    [frontend/movieDetailView] show movie details

--- frontend/src/views/MovieDetailView.vue
+++ frontend/src/views/MovieDetailView.vue
@@ -5,10 +5,28 @@
       color="red"
       v-if="loading"
     ></v-progress-circular>
-    <v-card>
+    <v-card v-show="!loading">
       <v-card-title>{{ movie.title }}</v-card-title>
       <v-card-subtitle>{{ movie.year }}</v-card-subtitle>
-      <v-img :src="movie.poster" />
+      <v-img aspect-ratio="1.7" :src="movie.poster" contain />
+      <v-container>
+        <label for="">Average Rating</label>
+        <span class="text-caption mr-2">
+          ({{ movie.avg_rating | decimalPlace }})
+        </span>
+        <v-rating v-model="movie.avg_rating" length="10" readonly></v-rating>
+      </v-container>
+      <v-container v-show="isLoggedIn">
+        <label for="">Estimated Rating</label>
+        <span class="text-caption mr-2">
+          ({{ movie.estimated_rating | decimalPlace }})
+        </span>
+        <v-rating
+          v-model="movie.estimated_rating"
+          length="10"
+          readonly
+        ></v-rating>
+      </v-container>
       <v-card-text>{{ movie.plot }}</v-card-text>
     </v-card>
   </v-container>
@@ -16,6 +34,7 @@
 
 <script>
 import MoviesService from "@/services/MoviesService";
+import { mapGetters } from "vuex";
 
 export default {
   name: "MovieView",
@@ -23,6 +42,9 @@ export default {
   props: { id: { required: true } },
   data() {
     return { movie: {}, loading: false };
+  },
+  computed: {
+    ...mapGetters("auth", ["isLoggedIn"]),
   },
   async created() {
     await this.loadMovie();
@@ -34,6 +56,11 @@ export default {
         .then((data) => (this.movie = data))
         .then(() => (this.loading = false))
         .catch((err) => alert(err.response.data));
+    },
+  },
+  filters: {
+    decimalPlace(num) {
+      return parseFloat(num).toFixed(2);
     },
   },
 };
