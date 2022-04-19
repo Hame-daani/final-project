c3bfe8d81172fb1141c0b30b4b207fe40dc2d5e0
Sadegh Hamedani <sad3gh.hmp@gmail.com>
2022-04-19 22:38:09 +0430

    [frontend/dashboard] myReviews added

--- frontend/src/components/MyProfile.vue
+++ frontend/src/components/MyProfile.vue
@@ -1,42 +1,40 @@
 <template>
-  <v-container>
-    <v-card v-if="!editing">
-      <v-card-title> {{ user.username }} </v-card-title>
-      <v-card-text>
-        Name: {{ user.first_name }} {{ user.last_name }}
-      </v-card-text>
-      <v-card-text> Email: {{ user.email }} </v-card-text>
-      <v-card-text> Gender: {{ user.gender }} </v-card-text>
-      <v-btn color="info" @click="enableEditing">Edit</v-btn>
-    </v-card>
-    <v-card v-else>
-      <v-card-title> Editing Your Info </v-card-title>
-      <template>
-        <v-form ref="form">
-          <v-text-field
-            label="First Name"
-            v-model="me.first_name"
-            hide-details="auto"
-            required
-          ></v-text-field>
-          <v-text-field
-            label="Last Name"
-            v-model="me.last_name"
-            hide-details="auto"
-            required
-          ></v-text-field>
-          <v-text-field
-            label="Email"
-            v-model="me.email"
-            hide-details="auto"
-            required
-          ></v-text-field>
-          <v-btn @click="update" color="info"> Update </v-btn>
-          <v-btn @click="cancel" color="yellow"> Cancel </v-btn>
-        </v-form>
-      </template>
-    </v-card>
-  </v-container>
+  <v-card v-if="!editing">
+    <v-card-title> {{ user.username }} </v-card-title>
+    <v-card-text>
+      Name: {{ user.first_name }} {{ user.last_name }}
+    </v-card-text>
+    <v-card-text> Email: {{ user.email }} </v-card-text>
+    <v-card-text> Gender: {{ user.gender }} </v-card-text>
+    <v-btn color="info" @click="enableEditing">Edit</v-btn>
+  </v-card>
+  <v-card v-else>
+    <v-card-title> Editing Your Info </v-card-title>
+    <template>
+      <v-form ref="form">
+        <v-text-field
+          label="First Name"
+          v-model="me.first_name"
+          hide-details="auto"
+          required
+        ></v-text-field>
+        <v-text-field
+          label="Last Name"
+          v-model="me.last_name"
+          hide-details="auto"
+          required
+        ></v-text-field>
+        <v-text-field
+          label="Email"
+          v-model="me.email"
+          hide-details="auto"
+          required
+        ></v-text-field>
+        <v-btn @click="update" color="info"> Update </v-btn>
+        <v-btn @click="cancel" color="yellow"> Cancel </v-btn>
+      </v-form>
+    </template>
+  </v-card>
 </template>
 
 <script>
--- frontend/src/components/MyReviews.vue
+++ frontend/src/components/MyReviews.vue
@@ -1,11 +1,68 @@
 <template>
-  <v-card>
-    <v-card-text> my reviews </v-card-text>
-  </v-card>
+  <v-container>
+    <v-card>
+      <v-card-title> My Reviews </v-card-title>
+      <loading-circular :flag="loading" />
+      <review-preview
+        v-for="review in reviews"
+        :key="review.id"
+        :review="review"
+      />
+    </v-card>
+    <v-pagination
+      v-model="page"
+      class="my-4"
+      :length="total_pages"
+      @input="loadReviews"
+    />
+  </v-container>
 </template>
 
 <script>
-export default {};
+import ReviewsService from "@/services/ReviewsService";
+import LoadingCircular from "@/components/LoadingCircular.vue";
+import ReviewPreview from "@/components/ReviewPreview.vue";
+
+export default {
+  components: {
+    LoadingCircular,
+    ReviewPreview,
+  },
+  props: {
+    user: { required: true },
+  },
+  async created() {
+    console.log("my reviews");
+    return this.loadReviews();
+  },
+  data() {
+    return {
+      reviews: [],
+      page: 1,
+      total_pages: 1,
+      loading: false,
+    };
+  },
+  methods: {
+    async loadReviews() {
+      this.loading = true;
+      this.reviews = [];
+      const payload = {
+        params: {
+          user: this.user.id,
+          page: this.page,
+        },
+      };
+      return ReviewsService.getReviews(payload)
+        .then((data) => {
+          this.reviews = data.results;
+          this.total_pages = data.total_pages;
+        })
+        .then(() => (this.loading = false))
+        .catch((err) => console.log(err.reponse.data));
+    },
+  },
+};
 </script>
 
 <style>
