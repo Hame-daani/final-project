b8ae280c8527cab2a0e30dbf30fbb668cdaa8399
Sadegh Hamedani <sad3gh.hmp@gmail.com>
2022-04-23 12:20:31 +0430

    [backend/migrations] fixed bug in one sided friendship

--- backend/app/migrations/0009_load_users_extra_data.py
+++ backend/app/migrations/0009_load_users_extra_data.py
@@ -3,21 +3,21 @@ from app.utils import timed
 import pandas as pd
 import ast
 from tqdm import tqdm
+from app.models import User
 
 
 @timed
 def add_friends(apps, schema):
     df = pd.read_csv("data/users.csv", low_memory=False)
-    User = apps.get_model("app", "User")
+    # User = apps.get_model("app", "User")
     for index, row in tqdm(df.iterrows(), total=df.shape[0]):
         user = User.objects.get(id=row["id"])
         friends = ast.literal_eval(row["friends"])
         user.friends.add(*friends)
-        # watchlist = ast.literal_eval(row["watchlist"])
-        # user.watchlist.add(*watchlist)
+        watchlist = ast.literal_eval(row["watchlist"])
+        user.watchlist.add(*watchlist)
         # favorites = ast.literal_eval(row["favorites"])
         # user.favorites.add(*favorites)
-        user.save()
 
 
 @timed
@@ -25,9 +25,8 @@ def removing_friends(apps, schema):
     User = apps.get_model("app", "User")
     for user in tqdm(User.objects.all()):
         user.friends.clear()
-        user.favorites.clear()
+        # user.favorites.clear()
         user.watchlist.clear()
-        user.save()
 
 
 class Migration(migrations.Migration):
