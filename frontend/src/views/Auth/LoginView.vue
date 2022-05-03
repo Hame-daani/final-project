c2b54cd3a72979029cc726c881fd6d9377a753f6
Sadegh Hamedani <sad3gh.hmp@gmail.com>
2022-05-03 11:40:01 +0430

    [frontend/login] cleanup and add alert

--- frontend/src/views/Auth/LoginView.vue
+++ frontend/src/views/Auth/LoginView.vue
@@ -1,24 +1,54 @@
 <template>
-  <v-containter>
-    <v-card max-width="500">
+  <v-container class="d-flex flex-column align-center justify-center">
+    <v-card width="500">
       <v-toolbar flat color="blue-grey" dark>
         <v-toolbar-title>Login</v-toolbar-title>
       </v-toolbar>
 
       <v-card-text>
-        <v-text-field filled label="Username" :value="username"></v-text-field>
-        <v-text-field filled label="Passwrod" :value="password"></v-text-field>
+        <v-text-field
+          filled
+          label="Username"
+          v-model="username"
+          type="username"
+        ></v-text-field>
+        <v-text-field
+          filled
+          label="Passwrod"
+          v-model="password"
+          type="password"
+          @keyup.enter="login"
+        ></v-text-field>
       </v-card-text>
 
       <v-divider></v-divider>
 
       <v-card-actions>
         <v-spacer></v-spacer>
-        <v-btn color="error" depressed> Clear </v-btn>
+        <v-btn color="error" depressed @click="clear"> Clear </v-btn>
         <v-btn color="success" depressed @click="login"> Login </v-btn>
       </v-card-actions>
     </v-card>
-  </v-containter>
+    <!-- <v-snackbar v-model="snackbar">
+      {{ error }}
+
+      <template v-slot:action="{ attrs }">
+        <v-btn color="blue" text v-bind="attrs" @click="snackbar = false">
+          Close
+        </v-btn>
+      </template>
+    </v-snackbar> -->
+    <v-alert
+      v-model="alert"
+      class="mt-5"
+      prominent
+      type="error"
+      width="500"
+      dismissible
+    >
+      {{ error }}
+    </v-alert>
+  </v-container>
 </template>
 
 <script>
@@ -27,21 +57,32 @@ export default {
     return {
       username: "",
       password: "",
-      errors: [],
+      alert: false,
+      error: "",
+      timeout: 2000,
     };
   },
   methods: {
     async login() {
       const payload = {
-        // username: this.username,
-        // password: this.password,
-        username: "EricWeber4298",
-        password: "12345678",
+        username: this.username,
+        password: this.password,
+        // username: "EricWeber4298",
+        // password: "12345678",
       };
       return this.$store
         .dispatch("auth/login", payload)
         .then(() => this.$router.push({ name: "dashboard" }))
-        .catch((err) => alert(err.response.data.non_field_errors));
+        .catch((err) => {
+          this.error = "";
+          for (const [key, value] of Object.entries(err.response.data))
+            this.error += `${key}: ${value} \n`;
+          this.alert = true;
+        });
+    },
+    clear() {
+      this.username = "";
+      this.password = "";
     },
   },
 };
