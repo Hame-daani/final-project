e57e55861153a38c6fe8e1162991f754a705780b
Sadegh Hamedani <sad3gh.hmp@gmail.com>
2022-05-04 12:51:06 +0430

    [frontend/home] put recommendations into panel

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,50 +1,103 @@
 <template>
   <v-container>
     <v-card class="my-50">
+      <v-expansion-panels accordion v-model="panelIndex" v-if="isLoggedIn">
+        <v-expansion-panel>
+          <v-expansion-panel-header>
+            Global Recommendations
+          </v-expansion-panel-header>
+          <v-expansion-panel-content>
+            <v-card class="my-50" :loading="globalLoading">
+              <v-toolbar flat color="blue-grey" dark>
+                <v-toolbar-title>Global Recommendation</v-toolbar-title>
+              </v-toolbar>
+              <template slot="progress">
+                <v-progress-linear
+                  color="deep-purple"
+                  height="10"
+                  indeterminate
+                  rounded
+                ></v-progress-linear>
+              </template>
+              <v-slide-group show-arrows>
+                <v-slide-item v-for="movie in globalRecs" :key="movie.id">
+                  <movie-preview :movie="movie" />
+                </v-slide-item>
+              </v-slide-group>
+            </v-card>
+          </v-expansion-panel-content>
+        </v-expansion-panel>
+        <v-expansion-panel>
+          <v-expansion-panel-header>
+            Friends Recommendations
+          </v-expansion-panel-header>
+          <v-expansion-panel-content>
+            <v-card class="my-50" :loading="friendsLoading">
+              <v-toolbar flat color="blue-grey" dark>
+                <v-toolbar-title>Friends Recommendation</v-toolbar-title>
+              </v-toolbar>
+              <template slot="progress">
+                <v-progress-linear
+                  color="deep-purple"
+                  height="10"
+                  indeterminate
+                  rounded
+                ></v-progress-linear>
+              </template>
+              <v-slide-group show-arrows>
+                <v-slide-item v-for="movie in friendsRecs" :key="movie.id">
+                  <movie-preview :movie="movie" />
+                </v-slide-item>
+              </v-slide-group>
+            </v-card>
+          </v-expansion-panel-content>
+        </v-expansion-panel>
+      </v-expansion-panels>
+    </v-card>
+
+    <v-card class="ma-10" :loading="recentReviewsLoading">
       <v-toolbar flat color="blue-grey" dark>
         <v-toolbar-title>Recent Reviews</v-toolbar-title>
       </v-toolbar>
-      <loading-circular :flag="recentReviewsLoading" />
-      <v-slide-group v-model="slide" show-arrows>
-        <v-slide-item v-for="review in recentReviews" :key="review.id">
+      <template slot="progress">
+        <v-progress-linear
+          color="deep-purple"
+          height="10"
+          indeterminate
+          rounded
+        ></v-progress-linear>
+      </template>
+      <v-slide-group show-arrows>
+        <v-slide-item
+          class="me-3"
+          v-for="review in recentReviews"
+          :key="review.id"
+        >
           <review-preview :review="review" />
         </v-slide-item>
       </v-slide-group>
     </v-card>
 
     <template v-if="isLoggedIn">
-      <v-card class="my-50">
+      <v-card class="ma-10" :loading="friendsReviewsLoading">
         <v-toolbar flat color="blue-grey" dark>
           <v-toolbar-title>Friends Reviews</v-toolbar-title>
         </v-toolbar>
-        <loading-circular :flag="friendsReviewsLoading" />
-        <v-slide-group v-model="slide" show-arrows>
-          <v-slide-item v-for="review in friendsRecentReviews" :key="review.id">
+        <template slot="progress">
+          <v-progress-linear
+            color="deep-purple"
+            height="10"
+            indeterminate
+            rounded
+          ></v-progress-linear>
+        </template>
+        <v-slide-group show-arrows>
+          <v-slide-item
+            class="me-3"
+            v-for="review in friendsRecentReviews"
+            :key="review.id"
+          >
             <review-preview :review="review" />
-          </v-slide-item>
-        </v-slide-group>
-      </v-card>
-
-      <v-card class="my-50">
-        <v-toolbar flat color="blue-grey" dark>
-          <v-toolbar-title>Global Recommendation</v-toolbar-title>
-        </v-toolbar>
-        <loading-circular :flag="globalLoading" />
-        <v-slide-group v-model="slide" show-arrows>
-          <v-slide-item v-for="movie in globalRecs" :key="movie.id">
-            <movie-preview :movie="movie" />
-          </v-slide-item>
-        </v-slide-group>
-      </v-card>
-
-      <v-card class="my-50">
-        <v-toolbar flat color="blue-grey" dark>
-          <v-toolbar-title>Friends Recommendation</v-toolbar-title>
-        </v-toolbar>
-        <loading-circular :flag="friendsLoading" />
-        <v-slide-group v-model="slide" show-arrows>
-          <v-slide-item v-for="movie in friendsRecs" :key="movie.id">
-            <movie-preview :movie="movie" />
           </v-slide-item>
         </v-slide-group>
       </v-card>
@@ -55,7 +108,6 @@
 <script>
 import { mapGetters } from "vuex";
 import ReviewPreview from "@/components/ReviewPreview.vue";
-import LoadingCircular from "@/components/LoadingCircular.vue";
 import MoviePreview from "@/components/MoviePreview.vue";
 import ReviewsService from "@/services/ReviewsService";
 import RecommenderService from "@/services/RecommenderService";
@@ -64,7 +116,6 @@ export default {
   components: {
     ReviewPreview,
     MoviePreview,
-    LoadingCircular,
   },
   data() {
     return {
@@ -77,15 +128,26 @@ export default {
       globalRecs: [],
       friendsRecs: [],
       slide: null,
+      panelIndex: -1,
     };
   },
   async created() {
-    await this.loadRecentReviews();
+    this.loadRecentReviews();
     if (this.isLoggedIn) {
-      await this.loadFriendsReviews();
-      await this.loadGlobalRecs();
-      await this.loadFriendsRecs();
+      this.loadFriendsReviews();
+      // await this.loadGlobalRecs();
+      // await this.loadFriendsRecs();
     }
+  },
+  watch: {
+    panelIndex: function () {
+      if (this.panelIndex === 0) {
+        this.loadGlobalRecs();
+      }
+      if (this.panelIndex === 1) {
+        this.loadFriendsRecs();
+      }
+    },
   },
   computed: {
     ...mapGetters("auth", ["isLoggedIn"]),
